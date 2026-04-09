c8c4b7d7f50efe9814eaa6e19675f838ea2d1cef
aiyah-meloken <aiyah_meloken@protonmail.com>
2026-04-09 16:36:54 +0800

    fix: restore main.tsx, remove preview import

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import CardFormatPreview from "./CardFormatPreview";
 import { AuthProvider } from "./lib/authStore";
 
-const isPreview = window.location.search.includes("preview");
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  isPreview ? (
-    <CardFormatPreview />
-  ) : (
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  ),
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
 );
